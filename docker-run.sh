c5677f050b218600499c3cf4f76ca76ea3a849ea
Van Hoang Kha <115985856+vanhoangkha@users.noreply.github.com>
2023-09-03 17:20:11 +0700

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,7 @@
 # Read input from file
 source input.txt
 
-# Delete all docker images
+# Delete all Docker images
 sudo docker system prune -a
 
 # Authenticate Docker client to your registry
@@ -12,9 +12,9 @@ aws ecr get-login-password --region "$REGION" | docker login --username AWS --pa
 # Build and tag Docker image
 docker build -t "$DOCKER_IMAGE" .
 
-# Tag
-
+# Tag the Docker image
 docker tag "$DOCKER_IMAGE":latest "$ACCOUNT".dkr.ecr."$REGION".amazonaws.com/"$DOCKER_IMAGE":latest
 
-# Push Docker image to repository
+# Push Docker image to the repository
 docker push "$ACCOUNT".dkr.ecr."$REGION".amazonaws.com/"$DOCKER_IMAGE":latest
+
